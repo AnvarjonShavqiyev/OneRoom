b34e7378c5e001e87546de0bf0ef9e403f98e9e7
GITHUBDAGI <129954447+watshihaharuki@users.noreply.github.com>
2024-02-01 10:25:08 +0500

    auth style completed

--- client/src/pages/auth/Auth.tsx
+++ client/src/pages/auth/Auth.tsx
@@ -1,6 +1,27 @@
-const Auth:React.FC = () => {
+import { Link, useParams } from "react-router-dom";
+import './Auth.scss'
+import Login from "./login/Login";
+import SignUp from "./signUp/SignUp";
+
+const Auth : React.FC = () => {
+  const {action} = useParams();
   return (
-    <div>Auth</div>
+    <div>
+        <div className="auth__wrapper">
+          <div className="auth-wrapper">
+            <h3 className="auth-title">My account</h3>
+            <div className="auth-form-wrapper">
+            <div className="auth__header">
+              <Link to='/auth/login' className={action === 'login' ? 'auth-action-title active' : 'auth-action-title'}>Login</Link>
+              <Link to='/auth/signup' className={action === 'signup' ? 'auth-action-title active' : 'auth-action-title'}>Create account</Link>
+            </div>
+            {
+              action === 'login' ? <Login/> : <SignUp/>
+            }
+            </div>
+          </div>
+        </div>
+     </div>
   )
 }
 
